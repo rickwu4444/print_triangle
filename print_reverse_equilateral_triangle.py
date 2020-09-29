bd26ceedaaf21a041c7a42976a618ef08260d7e2
rickwu4444 <rick0618@gmail.com>
2020-09-30 07:20:59 +0800

    print space

--- print_reverse_equilateral_triangle.py
+++ print_reverse_equilateral_triangle.py
@@ -1,20 +1,18 @@
 #coding=utf-8
 
 inputnum = int(input('Please input number of layer : '))
-
-s = ' '
 for x in range(inputnum,0,-1):
     if x == 0:
-        print(s*(inputnum-x)+'*')
+        print(' '*(inputnum-x)+'*')
     else:
         plist = [*range(0,x*2,1)]
-        print(s*(inputnum-x),end='')
+        print(' '*(inputnum-x),end='')
         for z in plist:
             if z == 0:
                 print('*',end='')
             elif z%2 == 0:
                 print('*',end='')
             elif z == plist[-1]:
-                print('')
+                print(' ')
             else:
-                print(s,end='')
+                print(' ',end='')
